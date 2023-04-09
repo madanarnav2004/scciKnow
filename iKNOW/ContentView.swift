84189c450ee7b95c6e70b95df644509e52da0b57
arnavmadan <45400804+arnavmadan@users.noreply.github.com>
2023-04-09 13:48:18 +0530

    Initial Commit

---
+++ iKNOW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iKNOW
+//
+//  Created by arnav on 09/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iKNOW/iKNOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKNOWApp.swift
+//  iKNOW
+//
+//  Created by arnav on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iKNOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
